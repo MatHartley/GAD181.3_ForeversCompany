641ca125abb4e60099e5996313fbf51d4c5f0a96
MatHartley_HomePC <115046381+MatHartley@users.noreply.github.com>
2023-11-24 12:45:12 +1100

    24/11/23
    Set up checkpoint prefab
    Set player shot time to 1
    Made new sprites for tile puzzle
    Put tile puzzle in game - functionality not implemented

--- Assets/Scripts/checkpoints/CheckPoint.cs
+++ Assets/Scripts/checkpoints/CheckPoint.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
-
+    public GameObject skullLight;
     private GameMaster gm;
 
     private void Start()
     {
         gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
     }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player1") || other.CompareTag("Player2"))
         {
             gm.lastCheckPointPos = transform.position;
+            skullLight.SetActive(true);
         }
     }
 }

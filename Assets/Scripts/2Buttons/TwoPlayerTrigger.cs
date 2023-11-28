84acf6f5e498775eec2b09a3d67f8bf19930fe21
MatthewGSAE <136556271+MatthewGSAE@users.noreply.github.com>
2023-11-28 21:53:30 +1100

    28/11/23
    working on the player specific areas, bit of trouble atm

--- Assets/Scripts/2Buttons/TwoPlayerTrigger.cs
+++ Assets/Scripts/2Buttons/TwoPlayerTrigger.cs
@@ -4,43 +4,26 @@ using UnityEngine;
 
 public class TwoPlayerTrigger : MonoBehaviour
 {
-    public KeyCode player1Key = KeyCode.E;
-    public KeyCode player2Key = KeyCode.Alpha7;
-
-    private bool player1KeyPressed = false;
-    private bool player2KeyPressed = false;
-
     private bool player1Inside = false;
     private bool player2Inside = false;
-
-    public Collider2D player1TriggerZone;
-    public Collider2D player2TriggerZone;
 
     public GameObject door1;
     public GameObject door2;
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (player1Inside &&  player2Inside)
+        if (player1Inside && player2Inside)
         {
-             if (Input.GetKeyDown(player1Key))
-             {
-                player1KeyPressed = true;
-                Debug.Log("E pressed");
-             }
-             if (Input.GetKeyDown(player2Key))
-             { 
-                player2KeyPressed = true;
-                Debug.Log("7 pressed");
-             }
-             if (player1KeyPressed && player2KeyPressed)
-             {
-                PerformAction();
-                Debug.Log("Performing action");
-             }
+            if (Input.GetKeyDown(KeyCode.E) && Input.GetKeyDown(KeyCode.Keypad7))
+            {
+                OpenDoors();
+            }
         }
-
+        else
+        {
+            Debug.Log("Waiting on button clicks");
+        }
     }
 
     // OnTriggerEnter2D is called when a collider enters the trigger zone
@@ -70,7 +53,7 @@ public class TwoPlayerTrigger : MonoBehaviour
     }
 
     // Perform the action when both players press their keys
-    private void PerformAction()
+    private void OpenDoors()
     {
         // Replace this with the action you want to perform
         Debug.Log("Both players pressed their keys inside their respective trigger zones. Performing action!");

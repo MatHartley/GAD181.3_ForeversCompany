ee7fb74544acc657edb4834d858c7a2720a05c03
MatthewGSAE <136556271+MatthewGSAE@users.noreply.github.com>
2023-11-28 22:13:18 +1100

    28/11/23
    still working on the walls, still no luck

--- Assets/Scripts/colour areas/BlockPlayer2.cs
+++ Assets/Scripts/colour areas/BlockPlayer2.cs
@@ -1,29 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class BlockPlayer2 : MonoBehaviour
 {
-    public PlayerController controller;
-    public Rigidbody2D playerBeingBlocked;
+    public GameObject player1;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (other.CompareTag("Player1"))
+        if (CompareTag("Player1"))
         {
             Debug.Log("Player1 can pass through the wall.");
+
+            Collider2D player1Collider = player1.GetComponent<BoxCollider2D>();
+            if (player1Collider != null)
+            {
+                player1Collider.isTrigger = true;
+            }
         }
-        else if (other.CompareTag("Player2"))
+        else if (CompareTag("Player2"))
         {
             Debug.Log("Player2 cannot pass through the wall.");
-            controller.speed = 0;
         }
     }
-    private void OnTriggerExit2D(Collider2D other)
+
+    private void OnCollisionExit2D(Collision2D collision)
     {
-        if (other.CompareTag("Player2"))
         {
-            controller.speed = 5;
+            if (CompareTag("Player1"))
+            {
+                Collider2D player1Collider = player1.GetComponent<BoxCollider2D>();
+                if (player1Collider != null)
+                {
+                    player1Collider.isTrigger = false;
+                }
+            }
         }
     }
 }

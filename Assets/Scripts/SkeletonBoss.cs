296b90c30ee00b7368e720e4aa1df6b1bc7dc5e2
MatHartley_HomePC <115046381+MatHartley@users.noreply.github.com>
2023-12-02 14:55:46 +1100

    2/11/23
    SkeletonBoss mats and lights

---
+++ Assets/Scripts/SkeletonBoss.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// swaps colour every X seconds, can only damage and be damaged by the opposite colour - ie when blue can only damage and be damaged by yellow
+/// gets stunned when takes damage, then swaps colour
+/// only takes damage from behind?
+/// takes Y hits to kill
+/// killing triggers end game sequence
+/// </summary>
+public class SkeletonBoss : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
